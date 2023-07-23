d44906852afcecf6437b42a1de0e5dba0857f475
DonDheri <lukas.lilholt@gmail.com>
2023-07-23 15:36:48 +0200

    Overførsel af filer

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -6,13 +6,8 @@
       <img src="@/assets/images/header.jpg" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center opacity-60" />
       <div class="mx-auto max-w-7xl px-6 lg:px-8">
         <div class="mx-auto max-w-2xl lg:mx-0">
-          <h2 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">The Sport Site</h2>
+          <h2 class="text-4xl font-bold tracking-tight text-white sm:text-6xl underline">The Sport Site</h2>
         </div>
-        <!-- <div class="mx-auto mt-28 max-w-2xl lg:mx-0 lg:max-w-none">
-          <div class="grid grid-cols-1 gap-x-8 gap-y-6 text-base font-semibold leading-7 text-white text-2xl sm:grid-cols-2 md:flex lg:gap-x-10">
-            <a v-for="link in links" :key="link.name" :href="link.href">{{ link.name }} <span aria-hidden="true">&rarr;</span></a>
-          </div>
-        </div> -->
       </div>
     </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,33 @@
 module.exports = {
   content: [
     "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.{vue}",
-    "./pages/**/*.{vue}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
     "./plugins/**/*.{js,ts}",
     "./nuxt.config.{js,ts}",
     "./app.vue",
     './assets/css/main.css',
   ],
   theme: {
-    extend: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
     },
+    
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  },
+  variants: {
+    backgroundColor: ["active"]
   },
   plugins: [],
 }
