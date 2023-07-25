38fb23749e58763ebb89e0fcbe122f3ae5d98a1a
DonDheri <lukas.lilholt@gmail.com>
2023-07-25 17:44:48 +0200

    kunne ikke finde ud af at fikse api...

--- components/content/DifferentSports.vue
+++ components/content/DifferentSports.vue
@@ -11,8 +11,7 @@
     <div class="flex flex-col text-center w-full justify-content mt-10">
         <div class="border-2 mx-48 flex flex-row-1 overflow-scroll px-10 py-8 rounded-xl">
             
-            <button 
-            v-for="(sport, index) in sports" :key="index" class="bg-gradient-to-r from-blue-500 to-blue-600 rounded px-5 py-2 bg-zinc-600 mx-6 text-white">
+            <button v-for="(sport) in sports" :key="sport.id" class="bg-gradient-to-r from-blue-500 to-blue-600 rounded px-5 py-2 bg-zinc-600 mx-6 text-white">
                 <a 
                     :href="`/content/${ sport.url }`"
                     
--- pages/content/basket-ball.vue
+++ pages/content/basket-ball.vue
@@ -3,17 +3,15 @@
     import TheFooter from '../../components/partials/TheFooter.vue';
     import DifferentSports from '../../components/content/DifferentSports.vue';
 
-    const id  = DifferentSports.key;
+    const APIKey = "1adcd8e27bmsh079f7bf7c1d6bfdp15a67ajsn9e21a6ac31c1"
+
+    const { id } = useRoute().params;
     const requestId = id as string || "";
 
-    const { data, pending, refresh } = await useAsyncData(requestId, () => $fetch(`https://api-basketball.p.rapidapi.com/teams/${requestId}`))
+    const { data } = await useFetch(`https://api-basketball.p.rapidapi.com/teams?league=12&season=2019-2020`)
 
-    const teams = computed(() => {
-        return data
-        
-    })
     
-    console.log(teams);
+    console.log(data);
     
 
 </script>
@@ -22,6 +20,12 @@
     <TheHeader/>
 
     <DifferentSports/>
+
+    <div class="grid grid-cols-4 gap-5">
+        <div v-for="d in data">
+            <p>{{ d }}</p>
+        </div>
+    </div>
 
     <TheFooter/>
 </template>
