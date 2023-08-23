19e0f0eed2ec62f94da35254d8cb7d562d18cd53
DonDheri <lukas.lilholt@gmail.com>
2023-08-23 14:14:04 +0200

    har fået deployed med netlify.

--- pages/content/basket-ball/teams/[id].vue
+++ pages/content/basket-ball/teams/[id].vue
@@ -40,40 +40,55 @@
     <div class="border-2 mx-48 grid px-10 place-items-center rounded-xl my-6 p-5">
         <p class="text-center w-full text-3xl border-b-2 pb-3">{{ team.team.name }}</p>
         <img class="w-[500px] my-5" :src="team.team.logo" alt="team logo">
-        <div class="text-2xl p-2 flex flex-row w-full rounded-xl">
-            <div class="flex flex-row">
-                <p class="m-3">Country: {{ team.country.name }} <img class="w-[45px]" :src="team.country.flag" alt="country flag"></p>
-                <p class="m-3">League: {{ team.league.name }} <img class="w-[50px]" :src="team.league.logo" alt="league logo"></p>
-                <p class="m-3">Games Played: <br>
-                    Home: {{ team.games.played.home }} <br>
-                    Away: {{ team.games.played.away }} <br>
-                    Total: {{ team.games.played.all }}</p>
-                <p class="m-3">Games Won: <br>
-                    Home: {{ team.games.wins.home.total }} <br>
-                    Away: {{ team.games.wins.away.total }} <br>
-                    Total: {{ team.games.wins.all.total }}
-                </p>
-                <p class="m-3">Games Lost: <br>
-                    Home: {{ team.games.loses.home.total }} <br>
-                    Away: {{ team.games.loses.away.total }} <br>
-                    Total: {{ team.games.loses.all.total }}
-                </p>
-                <p class="m-3">Games Draw: <br>
-                    Home: {{ team.games.draws.home.total }} <br>
-                    Away: {{ team.games.draws.away.total }} <br>
-                    Total: {{ team.games.draws.all.total }}
-                </p>
-                <p class="m-3">Points Scored: <br>
-                    Home: {{ team.points.for.total.home }} <br>
-                    Away: {{ team.points.for.total.away }} <br>
-                    Total: {{ team.points.for.total.all }}
-                </p>
-                <p class="m-3">Points Scored Against: <br>
-                    Home: {{ team.points.against.total.home }} <br>
-                    Away: {{ team.points.against.total.away }} <br>
-                    Total: {{ team.points.against.total.all }}
-                </p>
+        <div class="text-2xl p-2 grid grid-cols-8 place-items-center w-full border-b-2">
+            <p>Country:</p>
+            <p>League:</p>
+            <p>Games Played:</p>
+            <p>Games Won:</p>
+            <p>Games Lost:</p>
+            <p>Games Draw:</p>
+            <p>Points Scored:</p>
+            <p>Points Against:</p>
+        </div>
+        <div class="text-xl p-2 grid grid-cols-8 place-items-center w-full rounded-xl">
+            <div class="flex flex-col">
+                <p>{{ team.country.name }}</p>
+                <img class="w-[45px] place-self-center" :src="team.country.flag" alt="country flag">
             </div>
+            <div class="flex flex-col">
+                <p>{{ team.league.name }}</p>
+                <img class="w-[50px] place-self-center" :src="team.league.logo" alt="league logo">
+            </div>
+            <p>
+                Home: {{ team.games.played.home }} <br>
+                Away: {{ team.games.played.away }} <br>
+                Total: {{ team.games.played.all }}
+            </p>
+            <p>
+                Home: {{ team.games.wins.home.total }} <br>
+                Away: {{ team.games.wins.away.total }} <br>
+                Total: {{ team.games.wins.all.total }}
+            </p>
+            <p>
+                Home: {{ team.games.loses.home.total }} <br>
+                Away: {{ team.games.loses.away.total }} <br>
+                Total: {{ team.games.loses.all.total }}
+            </p>
+            <p>
+                Home: {{ team.games.draws.home.total }} <br>
+                Away: {{ team.games.draws.away.total }} <br>
+                Total: {{ team.games.draws.all.total }}
+            </p>
+            <p>
+                Home: {{ team.points.for.total.home }} <br>
+                Away: {{ team.points.for.total.away }} <br>
+                Total: {{ team.points.for.total.all }}
+            </p>
+            <p>
+                Home: {{ team.points.against.total.home }} <br>
+                Away: {{ team.points.against.total.away }} <br>
+                Total: {{ team.points.against.total.all }}
+            </p>
         </div>
     </div>
 </template>
