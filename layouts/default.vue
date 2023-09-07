0865d650b787e0fdd1ec8609809fd384fd6cd3f0
DonDheri <lukas.lilholt@gmail.com>
2023-09-07 14:59:09 +0200

    har fikset tab fanerne med animation

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,6 +4,7 @@
         { name: 'About', url: '/info/about', text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum fugiat possimus, aut enim provident distinctio excepturi deleniti est iure incidunt maiores nostrum adipisci rem esse cumque minima eveniet eum quo? Mollitia veniam asperiores ab illo delectus ex iste corporis autem, deleniti similique? Ab ut delectus dolores repudiandae placeat reprehenderit repellendus doloremque similique impedit explicabo, adipisci, error laborum iure necessitatibus soluta, minima asperiores quas quibusdam! Nihil explicabo, ratione quisquam doloremque fugit unde ad reiciendis vitae ex esse quam quae sit iure autem consequatur deleniti accusantium ea cupiditate vel nostrum numquam ipsum maiores? Quaerat voluptatem architecto corrupti sit minus sed dicta, cupiditate saepe ipsa ducimus cum! Id dignissimos consequuntur corrupti ea laboriosam, consequatur aut qui sunt, nobis impedit non temporibus porro ipsam error est possimus quasi eveniet rerum, sint iusto inventore sed quod nisi! Reprehenderit distinctio enim dolore beatae doloremque nemo officia eius quis sapiente nam, fugit unde ab error consequuntur officiis soluta quas fuga nisi vel. Ipsum delectus vel eveniet molestiae veniam optio molestias tenetur quis? Vel odio nesciunt assumenda nobis laborum quia quam beatae at veritatis itaque tempora blanditiis nihil corporis fugit reprehenderit minus, aperiam totam aliquam. Eos natus, ex fugiat officia ipsam ipsum pariatur earum et ad explicabo? Expedita.", },
         { name: 'Contact', url: '/info/contact', text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum fugiat possimus, aut enim provident distinctio excepturi deleniti est iure incidunt maiores nostrum adipisci rem esse cumque minima eveniet eum quo? Mollitia veniam asperiores ab illo delectus ex iste corporis autem, deleniti similique? Ab ut delectus dolores repudiandae placeat reprehenderit repellendus doloremque similique impedit explicabo, adipisci, error laborum iure necessitatibus soluta, minima asperiores quas quibusdam! Nihil explicabo, ratione quisquam doloremque fugit unde ad reiciendis vitae ex esse quam quae sit iure autem consequatur deleniti accusantium ea cupiditate vel nostrum numquam ipsum maiores? Quaerat voluptatem architecto corrupti sit minus sed dicta, cupiditate saepe ipsa ducimus cum! Id dignissimos consequuntur corrupti ea laboriosam, consequatur aut qui sunt, nobis impedit non temporibus porro ipsam error est possimus quasi eveniet rerum, sint iusto inventore sed quod nisi! Reprehenderit distinctio enim dolore beatae doloremque nemo officia eius quis sapiente nam, fugit unde ab error consequuntur officiis soluta quas fuga nisi vel. Ipsum delectus vel eveniet molestiae veniam optio molestias tenetur quis? Vel odio nesciunt assumenda nobis laborum quia quam beatae at veritatis itaque tempora blanditiis nihil corporis fugit reprehenderit minus, aperiam totam aliquam. Eos natus, ex fugiat officia ipsam ipsum pariatur earum et ad explicabo? Expedita.", },
     ]
+    const tabs = ref([])
     const sports = [
     {
         id: 0,
@@ -101,24 +102,33 @@
 
     // const clicked = (event: Event) => {
         
-    //     return event.target.classList.toggle("tab-active")
+    //     return event.target.classList.toggle("tabActive")
     // }
-    function deActivate(event: Event) {
+    function deActivate() {
         for (let sport of sports) {
             if (sport.active === true) {
-                return sport.active = false;
+                sport.active = false;
+                for (let tab of tabs.value) {
+                    if (tab.classList.contains("tabActive")) {
+                        tab.classList.remove("tabActive")
+                    }
+                }
             }
         }
     }
     const clicked = (event: Event) => {
-        
+        deActivate()
         for (let sport of sports) {
             if (event.target.id.toString() === sport.id.toString()) {
-                return sport.active = true
+                sport.active = true;
+                event.target.classList.add("tabActive")
             }
+
         }
         
     }
+    
+    
 </script>
 
 <template>
@@ -127,15 +137,16 @@
             <h2 class="text-4xl italic font-bold tracking-tight sm:text-3xl sm:h-[100px] md:h-[75px]">The Sports Site</h2>
         </div>
         <div class="bg-gray-950">
-            <p class=" text-2xl py-1 border-b-2 border-lime-500">All Sports</p>
-            <div class="tabs tabs-boxed place-content-center object-contain">
+            <p class="text-2xl py-1 border-b-2 border-lime-500">All Sports</p>
+            <div class="tabs carousel place-content-center flex-wrap">
                 <button
-                v-for="sport in sports"
-                @click="deActivate"
-                :id="`${sport.id}`"
-                :key="sport.url"
-                class="tab rounded-full text-lime-500 font-semibold uppercase flex-wrap"
-                :class="{ 'tab-lifted': sport.active === true }"
+                    v-for="sport, index in sports"
+                    @click="clicked"
+                    :id="sport.id.toString()"
+                    ref="tabs"
+                    :key="index"
+                    class="carousel-item tabLifted tab-md text-lime-500 font-semibold uppercase"
+                    :class="{ tabActive: sport.active === true }"
                 >{{ sport.title }}</button>
             </div>
         </div>
